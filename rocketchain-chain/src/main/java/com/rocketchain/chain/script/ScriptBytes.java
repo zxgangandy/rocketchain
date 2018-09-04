138c8563d4546e2fbd134894413962da07e9ee57
andy zhao <286505491@qq.com>
2018-09-04 23:57:14 +0800

    script;

---
+++ rocketchain-chain/src/main/java/com/rocketchain/chain/script/ScriptBytes.java
@@ -0,0 +1,38 @@
+package com.rocketchain.chain.script;
+
+import java.util.Arrays;
+
+public class ScriptBytes extends ScriptValue {
+
+    public ScriptBytes(byte[] bytesValue) {
+        value = bytesValue;
+    }
+
+
+    @Override
+    public ScriptValue copy() {
+        return new ScriptBytes(value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(value);
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == null) {
+            return false;
+        } else if (obj instanceof ScriptBytes) {
+            return ((ScriptBytes) obj).canEqual(this) && Arrays.equals(((ScriptBytes) obj).value, value);
+        } else {
+            return false;
+        }
+    }
+
+    boolean canEqual(Object obj) {
+        return obj instanceof ScriptBytes;
+    }
+}
---
+++ rocketchain-chain/src/main/java/com/rocketchain/chain/script/ScriptEnvironment.java
@@ -0,0 +1,39 @@
+package com.rocketchain.chain.script;
+
+import com.rocketchain.proto.Transaction;
+
+public class ScriptEnvironment {
+    private Transaction transaction ;
+    private int transactionInputIndex ;
+
+    public ScriptEnvironment(Transaction transaction, int transactionInputIndex) {
+        this.transaction = transaction;
+        this.transactionInputIndex = transactionInputIndex;
+    }
+
+    // BUGBUG : if OP_CHECKSIG or OP_CHECKMULTISIG runs without OP_CODESEPARATOR,
+    //          can we keep signatureOffset as zero?
+    // The offset in the raw script where the data for checking signature starts.
+    private int sigCheckOffset;
+
+    /** Set the offset of raw script where the data for checking signature starts.
+     *
+     * @param offset the offset of raw script
+     */
+    public void setSigCheckOffset(int offset ) {
+        sigCheckOffset = offset;
+    }
+
+    /** Get the offset of raw script where the data for checking signature starts.
+     *
+     * @return The offset.
+     */
+    public int getSigCheckOffset() {
+        return sigCheckOffset;
+    }
+
+    private ScriptStack stack = new  ScriptStack();
+    // The altStack is necessary to support OP_TOALTSTACK and OP_FROMALTSTACK,
+    // which moves items on top of the stack and the alternative stack.
+    private ScriptStack altStack = new  ScriptStack();
+}
