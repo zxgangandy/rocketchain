ad205439dffa85c3fd3eb647f0401b01bcd190ab
andy zhao <286505491@qq.com>
2018-10-09 23:05:12 +0800

    WalletOutput;

--- rocketchain-proto/src/main/java/com/rocketchain/proto/WalletOutput.java
+++ rocketchain-proto/src/main/java/com/rocketchain/proto/WalletOutput.java
@@ -3,7 +3,7 @@ package com.rocketchain.proto;
 public class WalletOutput implements Transcodable{
     // Some(block height) of the block on the local best block chain which includes this transaction. None otherwise.
     // BUGBUG : Change name to blockIndex
-    private long blockindex ;          // 11,
+    private Long blockindex ;          // 11,
     // Whether this output is in the generation transaction.
     private boolean coinbase ;
     // Whether this coin was spent or not.
@@ -11,14 +11,26 @@ public class WalletOutput implements Transcodable{
     // The transaction output
     private TransactionOutput transactionOutput ;
 
-    public WalletOutput(long blockindex, boolean coinbase, boolean spent, TransactionOutput transactionOutput) {
+    public WalletOutput(Long blockindex, boolean coinbase, boolean spent, TransactionOutput transactionOutput) {
         this.blockindex = blockindex;
         this.coinbase = coinbase;
         this.spent = spent;
         this.transactionOutput = transactionOutput;
     }
 
-    public long getBlockindex() {
+    public void setBlockindex(Long blockindex) {
+        this.blockindex = blockindex;
+    }
+
+    public void setCoinbase(boolean coinbase) {
+        this.coinbase = coinbase;
+    }
+
+    public void setTransactionOutput(TransactionOutput transactionOutput) {
+        this.transactionOutput = transactionOutput;
+    }
+
+    public Long getBlockindex() {
         return blockindex;
     }
 
